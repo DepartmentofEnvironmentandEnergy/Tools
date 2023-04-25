3ccd230b3d9b7147085e021eae22c5b4214c5368
DepartmentofEnvironmentandEnergy <109674863+DepartmentofEnvironmentandEnergy@users.noreply.github.com>
2023-04-25 13:00:15 -0500

    Update FindThingsFastByAusten.bat

--- FindThingsFastByAusten.bat
+++ FindThingsFastByAusten.bat
@@ -1,4 +1,29 @@
+@REM Code explanation:
+@REM This is a batch script that allows the user to search for files in a directory based on a specified file type, date range, and keyword. 
+@REM The first line "@echo off" turns off the command echo so that the commands executed in the script are not displayed in the command prompt.
+@REM The second line "title Created by Austen Green" sets the title of the command prompt window.
+@REM The third line "setlocal" limits the scope of variable changes to the current batch script.
+@REM The next four lines prompt the user to enter the search directory, file type, start date, end date, and keyword respectively. The "set /p" command allows the user to input the values and assigns them to variables.
+@REM The "start_time" variable is set to the current time.
+@REM The "for" loop searches for files in the specified directory and file type using the "findstr" command to search for the keyword in the file contents. If the keyword is found, the file path is printed. If the keyword is not found in the file contents, the file path is checked again using the "findstr" command to search for the keyword in the file name. If the keyword is found, the file path is printed.
+@REM The "end_time" variable is set to the current time.
+@REM The "time_diff" variable is calculated by converting the start and end times to seconds and subtracting them.
+@REM The last few lines print a message indicating that the search is complete and the time taken to complete the search. The "pause" command is used to keep the command prompt window open until the user presses a key.
+
+
+
+REM Example use:
+REM Enter the search directory (e.g. C:\Users\austen.green\Desktop): C:\Users\JohnDoe\Documents
+REM Enter the file type to search (e.g. *.txt): *.docx
+REM Enter the start date for the search (MM/DD/YYYY): 01/01/2021
+REM Enter the end date for the search (MM/DD/YYYY): 12/31/2021
+REM Enter the keyword to search for: confidential
+
+
+
 @echo off
+title Created by Austen Green
+
 setlocal
 
 set /p search_dir=Enter the search directory (e.g. C:\Users\austen.green\Desktop):
@@ -12,6 +37,10 @@ set "start_time=%time%"
 for /r %search_dir% %%f in (%file_type%) do (
     findstr /i /m /c:"%keyword%" "%%f" && (
         echo %%f
+    ) || (
+        echo %%f | findstr /i /c:"%keyword%" && (
+            echo %%f
+        )
     )
 )
 
@@ -32,12 +61,3 @@ echo         Search complete
 echo ....................................
 echo Time taken: %time_diff% seconds
 pause
-
-REM Example use:
-REM Enter the search directory (e.g. C:\Users\austen.green\Desktop): C:\Users\JohnDoe\Documents
-REM Enter the file type to search (e.g. *.txt): *.docx
-REM Enter the start date for the search (MM/DD/YYYY): 01/01/2021
-REM Enter the end date for the search (MM/DD/YYYY): 12/31/2021
-REM Enter the keyword to search for: confidential
-
-REM The script will then search for all .docx files containing the keyword "confidential" within the specified date range in the directory "C:\Users\JohnDoe\Documents". Once complete, it will display the time taken to complete the search.
